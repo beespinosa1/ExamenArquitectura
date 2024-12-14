4d0bbfe3267ab9b0677ed4b415ad204338ea7116
beespinosa1 <beespinosa1@espe.edu.ec>
2024-12-14 08:05:23 -0500

    first commit

---
+++ src/main/java/com/espinosa/examen/segmento/SegmentoApplication.java
@@ -0,0 +1,13 @@
+package com.espinosa.examen.segmento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegmentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegmentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espinosa/examen/segmento/SegmentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espinosa.examen.segmento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegmentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
